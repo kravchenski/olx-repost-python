93cf3e0ccd3f14e17275a277ece754c8f3018809
kravchenski <max5045955@gmail.com>
2025-07-26 20:30:45 +0200

    feat: set_timeouts to page

--- config/project_variables.py
+++ config/project_variables.py
@@ -4,5 +4,5 @@ from DrissionPage import ChromiumOptions
 po = ChromiumOptions()
 po.set_argument('--start-maximized')
 po.set_argument('--disable-infobars')
-
+po.set_timeouts(base=30, page_load=30, script=30)
 page = ChromiumPage(po)
