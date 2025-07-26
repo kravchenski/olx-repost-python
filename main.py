b3040ca19f1d2d4a8748312e5fef046b2d66c1cf
kravchenski <max5045955@gmail.com>
2025-07-26 20:33:07 +0200

    feat: create repost_ad_to_second_account with all functions

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
+from pages import login_page, save_ad_info, add_ad_to_second_account
+from utils import create_account_ad_json, add_labels_to_account_ad_json
 
-from pages import login_page, get_ads_info
+
+def repost_ad_to_second_account():
+    login_page(os.getenv('EMAIL_FIRST_ACCOUNT'), os.getenv('PASSWORD_FIRST_ACCOUNT'), False)
+    save_ad_info()
+    create_account_ad_json()
+    add_labels_to_account_ad_json()
+    login_page(os.getenv('EMAIL_SECOND_ACCOUNT'), os.getenv('PASSWORD_SECOND_ACCOUNT'), True)
+    add_ad_to_second_account()
+
 
 if __name__ == '__main__':
-    login_page(os.getenv('EMAIL_FIRST_ACCOUNT'), os.getenv('PASSWORD_FIRST_ACCOUNT'))
-    get_ads_info()
+    repost_ad_to_second_account()
