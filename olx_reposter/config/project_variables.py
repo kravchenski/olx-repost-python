5e6264e9aaf7f4be1282c727267593ef627dacd1
kravchenski <max5045955@gmail.com>
2025-08-07 20:13:56 +0200

    feat: add create_page func

--- olx_reposter/config/project_variables.py
+++ olx_reposter/config/project_variables.py
@@ -1,8 +1,10 @@
 from DrissionPage import ChromiumPage
 from DrissionPage import ChromiumOptions
 
-po = ChromiumOptions()
-po.set_argument('--start-maximized')
-po.set_argument('--disable-infobars')
-po.set_timeouts(base=30, page_load=30, script=30)
-page = ChromiumPage(po)
+
+def create_page():
+    po = ChromiumOptions().auto_port()
+    po.set_argument('--start-maximized')
+    po.set_argument('--disable-infobars')
+    po.set_timeouts(base=30, page_load=30, script=30)
+    return ChromiumPage(po)
