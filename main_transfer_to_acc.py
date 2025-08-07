a8f6474b81bb9794756aa2a5fa8e0bbf2e29c271
kravchenski <max5045955@gmail.com>
2025-08-07 21:28:44 +0200

    init: create main_transfer_to_acc example

---
+++ main_transfer_to_acc.py
@@ -0,0 +1,15 @@
+from olx_reposter import DatabaseModel, OlxAdReposter
+from olx_reposter.config.project_variables import emails, password
+
+olx_reposter = OlxAdReposter()
+
+
+async def main():
+    data_from_transfer_db = await DatabaseModel().select_all_rows_from_transfer_db()
+    for single_data in data_from_transfer_db:
+        if not single_data['is_transferred']:
+            await olx_reposter.transfer_ad_between_accounts(int(single_data['advertising_id']),
+                                                            emails[single_data['account_from'] - 1], password,
+                                                            emails[single_data['account_to'] - 1], password)
+        else:
+            continue
