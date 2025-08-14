7a73642734b7db2cbe2d43454e6083f8ca3aba89
kravchenski <max5045955@gmail.com>
2025-08-15 00:36:40 +0200

    feat: change app version

--- olx_reposter/__init__.py
+++ olx_reposter/__init__.py
@@ -3,5 +3,5 @@ from olx_reposter.models.OlxAdReposter import OlxAdReposter
 from olx_reposter.models.PostgresDBModel import PostgresDatabaseModel
 from olx_reposter.models.MySqlDBModel import MySQLDatabaseModel
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["OlxAdReposter", "AdDataExtractor", "PostgresDatabaseModel","MySQLDatabaseModel"]
