7315e065da7f6a09d9169fb14aef5a5cd7b9c142
kravchenski <max5045955@gmail.com>
2025-08-15 00:30:11 +0200

    feat: add mysql variables

--- olx_reposter/config/__init__.py
+++ olx_reposter/config/__init__.py
@@ -7,3 +7,8 @@ pg_host = os.getenv('POSTGRES_HOST')
 pg_user = os.getenv('POSTGRES_USER')
 pg_password = os.getenv('POSTGRES_PASSWORD')
 pg_database = os.getenv('POSTGRES_DATABASE')
+
+mysql_host = os.getenv('MYSQL_HOST')
+mysql_user = os.getenv('MYSQL_USER')
+mysql_password = os.getenv('MYSQL_PASSWORD')
+mysql_database = os.getenv('MYSQL_DATABASE')
